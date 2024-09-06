c1d99b5bc3cdfbb0faa230e30e9c4ebfa71c42af
Brenno Mayer <bmnyor@github.com>
2024-09-06 13:31:12 -0300

    Initial commit, started display component and started keyboard component

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import DisplayPanelComponent from "./components/DisplayPanelComponent.vue";
+import KeyboardComponent from "./components/KeyboardComponent.vue";
+
+</script>
+
+<template>
+  <DisplayPanelComponent/>
+  <KeyboardComponent/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
